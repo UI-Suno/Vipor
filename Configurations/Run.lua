f804b5909c446d43fdfc960ab57f2d8aa7b5ddf9
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 05:56:46 -0700

    Delete Run.lua

--- Configurations/Run.lua
+++
@@ -1,5 +0,0 @@
-task.wait(0.25)
-
-if game.PlaceId == 9346039031 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Suno/Vipor/main/Scripts/9346039031.lua', true))('9346039031')
-end
