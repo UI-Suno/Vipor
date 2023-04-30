9f47431285ddb8c93b69ef6238db50b56d62ec92
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 06:07:27 -0700

    Add files via upload

---
+++ Downloads/Library.lua
@@ -0,0 +1,21 @@
+task.wait(0.5)
+
+local Services = {
+    ["Players"] = game:GetService("Players"),
+}
+
+local LocalPlayer = Services.Players.LocalPlayer;
+
+if not writefile then
+    LocalPlayer:Kick("Your exploit does not support `filesystem`, maybe upgrade your exploit?")
+else
+    if not isfolder("Vipor") then
+        LocalPlayer:Kick("(E: 404) Vipor does not appear to exist within our database!")
+    else
+        if not isfolder("Vipor\\Downloads") then
+            LocalPlayer:Kick("(E: 404) Vipor/Configurations does not appear to exist within our database!")
+        else
+            writefile("Vipor\\Downloads\\Library.lua", "loadstring(game:HttpGet('https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt', true))()")
+        end
+    end
+end
