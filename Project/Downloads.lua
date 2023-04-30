a0d95d1f16a3eed014582cadbedae5cc5da609f2
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 02:58:49 -0700

    Create Downloads.lua

---
+++ Project/Downloads.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Information.lua", true))("Information")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Version.lua", true))("Version")
