d2a86f817246790d372f53d47bf3b67691469219
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 04:36:24 -0700

    Update Downloads.lua

--- Project/Downloads.lua
+++ Project/Downloads.lua
@@ -1,2 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Information.lua", true))("Information")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Library.lua", true))("Library")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Version.lua", true))("Version")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Developer.lua", true))("Developer")
