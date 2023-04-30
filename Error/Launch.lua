c727daeb5f4dd7b83708880763ad34a5e7a3c55d
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 06:12:05 -0700

    Add files via upload

---
+++ Error/Launch.lua
@@ -0,0 +1,4 @@
+if getgenv().Vipor == true then
+    return warn("Vipor is already running, please rejoin the experience to re-launch Vipor!")
+end
+getgenv().Vipor = true
---
+++ Project/Main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Error/Wait.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Error/Launch.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Configurations/System.lua", true))("System")
