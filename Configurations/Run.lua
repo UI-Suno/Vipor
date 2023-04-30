a5d68dc8c587bc6e41c367db1525fa8031c6d877
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 05:32:05 -0700

    Update Run.lua

--- Configurations/Run.lua
+++ Configurations/Run.lua
@@ -14,13 +14,19 @@ else
     else
         if not isfolder("Vipor\\Scripts") then
             LocalPlayer:Kick("(E: 404) Vipor/Configurations does not appear to exist within our database!")
-        else
-            writefile("Vipor\\Scripts\\9346039031.lua", "loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Suno/Vipor/main/Scripts/9346039031.lua', true))('9346039031')")
         end
     end
 end
 
-task.wait(0.25)
-if game.PlaceId == 9346039031 then
-    dofile("Vipor\\Scripts\\9346039031.lua")
+writefile("Vipor\\Scripts\\9346039031.lua", "loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Suno/Vipor/main/Scripts/9346039031.lua', true))('9346039031')")
+
+if not loadfile then
+    return
+end
+
+task.wait(0.5)
+do
+    if game.PlaceId == 9346039031 then
+        loadfile("Vipor\\Scripts\\9346039031.lua")
+    end
 end
