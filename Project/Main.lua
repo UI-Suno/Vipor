1c7273c9f421390905ae8beb64f7e65e03268e0b
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 06:32:32 -0700

    Delete Project directory

--- Project/Main.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Error/Wait.lua"))
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Error/Launch.lua"))
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Configurations/System.lua", true))("System")
