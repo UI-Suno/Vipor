6b3d9dd0bee6656a34e7774e37bfd40b1d9a926d
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 04:27:54 -0700

    Update Information.lua

--- Downloads/Information.lua
+++ Downloads/Information.lua
@@ -12,6 +12,24 @@ else
     if not isfolder("Vipor") then
         LocalPlayer:Kick("(E: 404) Vipor does not appear to exist within our database!")
     else
-        writefile("Vipor\\information.txt", "Vipor programmed by Suno#2182\nOpen-Sourced public project, you may use this code as you please!\n\nSkidded is allowed I guess, if you are brave enough to read all of this dogshit code, then go ahead and skid, I'm allowing you.\n\nVipor created by Suno#2182, No Rights Reserved, Open-Source\n\nAdd me on Discord if you have any questions, or just want to be friends! (Suno#2182 for the last time)")
+        writefile("Vipor\\Information.txt", [[
+* Vipor
+Programmed by Suno!
+Created as an Open-Sourced project!
+
+* Media
+https://linktr.ee/sunoui
+Discord: Suno#2182
+
+* Explanation
+Vipor uses Synapse's, or Script-Ware's FileSystem function to upload the files to your computer, and GitHub to actually store, and load the files between each other.
+I know Vipor's system could possibly be upgraded, but this is the best I could come up with.
+Since Lua is not my strongsuit when it comes to programming, please don't make fun of me for the bad coding, and no comments.
+
+* Use
+You may use this code freely, and you may look, or use the code.
+If you were to use the code, try to give me credits in any way, shape, or form.
+The code is 100% public, so there is no need to worry about copyright.
+        ]])
     end
 end
