ab185ae9228750e6749c968dfb3a0089c4b45e28
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-04-30 05:58:05 -0700

    Add files via upload

---
+++ Configurations/Run.lua
@@ -0,0 +1,5 @@
+task.wait(0.25)
+
+if game.PlaceId == 9346039031 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Suno/Vipor/main/Scripts/9346039031.lua', true))('9346039031')
+end
