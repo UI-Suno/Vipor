b3b3e0801b5d829e9ccb887e3a9ccf4554f49a84
Suno <130910887+UI-Suno@users.noreply.github.com>
2023-05-02 19:07:09 -0700

    Delete Project directory

--- Project/Downloads.lua
+++
@@ -1,5 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Information.lua", true))(
-    "Information"
-)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Version.lua", true))("Version")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Downloads/Library.lua", true))("Library")
--- Project/Main.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UI-Suno/Vipor/main/Configurations/System.lua", true))(
-    "System"
-)
